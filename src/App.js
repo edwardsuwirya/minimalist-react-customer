bd35968a789441cfc36b63eff00073ec2e79168f
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-21 08:07:17 +0700

    Change props to controller and service

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import CustomerService from "./services/customer/CustomerService";
 import useCustomerPageController from "./features/customer/customerPage/useCustomerPageController";
 import LoginPageView from "./features/login/loginPage/LoginPageView";
-import {useState} from "react";
 import useLoginPageController from "./features/login/loginPage/useLoginPageController";
 import LoginService from "./services/customer/LoginService";
 import {useSelector} from "react-redux";
 import {userIdSelector} from "./features/login/LoginSelector";
-import LoginPageView from "./features/login/loginPage/LoginPageView";
 import CustomerPageView from "./features/customer/customerPage/CustomerPageView";
 
 const App = () => {
     const userId = useSelector(userIdSelector)
     return (
         <>
-            {userId === ""  ?
-                <LoginPageView controller={() => LoginPageController(LoginService)} /> :
-                <CustomerPageView controller={() => CustomerPageController(CustomerService)}/>
+            {userId === "" ?
+                <LoginPageView controller={useLoginPageController} service={LoginService}/> :
+                <CustomerPageView controller={useCustomerPageController} service={CustomerService}/>
             }
         </>
     );
--- src/services/customer/LoginService.js
+++ src/services/customer/LoginService.js
@@ -1,15 +1,13 @@
 const LoginService = () => {
     const Auth = (userCred) => {
         return new Promise((resolve, reject) => {
-            if (userCred.userName === 'enigma' && userCred.password === '123456') {
+            if (userCred.userName === 'enigma' && userCred.password === '111') {
                 resolve('C001')
             } else {
                 reject('Unauthorized')
             }
-
         })
     }
-
 
     return {
         Auth
